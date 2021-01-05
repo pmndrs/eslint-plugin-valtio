c19e09d18933226dc000f99290187bf9dc8551f0
daishi <daishi@axlight.com>
2021-01-05 11:02:34 +0900

    fix: test

--- tests/StateSnapshot.test.js
+++ tests/StateSnapshot.test.js
@@ -3,16 +3,7 @@ import rule from '../src/StateSnapshot'
 
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } })
 
-describe('valid tests', () => {
-  it('create proxy', () => {
-    ruleTester.run('state-snapshot-rule', rule, {
-      valid: ['const state = proxy({ count: 0 }); state.count += 1'],
-    })
-  })
-
-  // TODO
-})
-
-describe('invalid tests', () => {
-  // TODO
+ruleTester.run('state-snapshot-rule', rule, {
+  valid: ['const state = proxy({ count: 0 }); state.count += 1'],
+  invalid: [],
 })
