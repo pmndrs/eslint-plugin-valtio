8a1be05bfce299b3f633e6ed14031ff616e3110a
M. Bagher Abiat <zorofight94@gmail.com>
2021-02-10 20:03:30 +0330

    fix failing tests

--- src/StateSnapshot.js
+++ src/StateSnapshot.js
@@ -6,62 +6,29 @@ export const SNAPSHOT_CALLBACK_MESSAGE = 'Better to just use proxy state'
 export const UNEXPECTED_STATE_MUTATING =
   'Unexpected state mutating( I think we have to change that'
 
-function isSameMemmberExpression(first, second) {
-  if (first.property.name === second.property.name) {
-    if (
-      first.object._babelType === 'MemberExpression' &&
-      second.object._babelType === 'MemberExpression'
-    ) {
-      return isSameMemmberExpression(first.object, second.object)
-    } else if (
-      first.object.type === 'Identifier' &&
-      second.object.type === 'Identifier'
-    ) {
-      return first.object.type === second.object.type
-    }
-  } else {
-    return false
+function outerMemberExpression(node) {
+  if (node.parent.type !== 'MemberExpression') {
+    return node
+  }
+  return outerMemberExpression(node.parent)
+}
+function isInSomething(node, thing) {
+  if (node.parent && node.parent.type !== thing) {
+    return isInSomething(node.parent, thing)
+  } else if (node.parent && node.parent.type === thing) {
+    return true
   }
   return false
 }
-function isUsedInUseProxy(node, scope) {
-  let isUsed = false
 
-  if (!scope) return isUsed
-
-  scope.variables.forEach((variable) => {
-    const def = variable.defs[0]
-    if (!def || isUsed) return
-
-    const init = def.node.init
-    if (!init || !init.arguments) return
-
-    if (
-      (init.parent._babelType === 'CallExpression' &&
-        init.parent.callee.name === 'useProxy') ||
-      (init._babelType === 'CallExpression' && init.callee.name === 'useProxy')
-    ) {
-      if (
-        init.arguments[0] &&
-        init.arguments[0]._babelType === 'MemberExpression' &&
-        node.parent._babelType === 'MemberExpression'
-      ) {
-        return (isUsed = isSameMemmberExpression(
-          node.parent.parent.left,
-          init.arguments[0]
-        ))
-      } else if (
-        init.arguments[0].type === 'Identifier' &&
-        node.type === 'Identifier' &&
-        node.parent.type !== 'MemberExpression'
-      ) {
-        return (isUsed = init.arguments[0].name === node.name)
-      }
-    }
-  })
-  if (!isUsed && scope.upper)
-    return (isUsed = isUsedInUseProxy(node, scope.upper))
-  return isUsed
+function isInMemberExpression(node) {
+  return isInSomething(node, 'MemberExpression')
+}
+function isInAssignmentExpression(node) {
+  return (
+    isInSomething(node, 'AssignmentExpression') ||
+    isInSomething(node, 'UpdateExpression')
+  )
 }
 
 function which(name, scope) {
@@ -90,6 +57,69 @@ function which(name, scope) {
 
   return kind
 }
+function isSameMemmberExpression(first, second) {
+  if (!first || !second) return false
+  if (
+    first.property.name === second.property.name ||
+    first.property.value === second.property.value
+  ) {
+    if (
+      first.object._babelType === 'MemberExpression' &&
+      second.object._babelType === 'MemberExpression'
+    ) {
+      return isSameMemmberExpression(first.object, second.object)
+    } else if (
+      first.object.type === 'Identifier' &&
+      second.object.type === 'Identifier'
+    ) {
+      return first.object.type === second.object.type
+    }
+  } else {
+    return false
+  }
+  return false
+}
+function isUsedInUseProxy(node, scope) {
+  let isUsed = false
+
+  console.log(node.property && node.property.type === 'Literal', node)
+  if (!scope) return isUsed
+
+  scope.variables.forEach((variable) => {
+    const def = variable.defs[0]
+    if (!def || isUsed) return
+
+    const init = def.node.init
+    if (!init || !init.arguments) return
+
+    if (
+      (init.parent._babelType === 'CallExpression' &&
+        init.parent.callee.name === 'useProxy') ||
+      (init._babelType === 'CallExpression' && init.callee.name === 'useProxy')
+    ) {
+      if (
+        init.arguments[0] &&
+        init.arguments[0]._babelType === 'MemberExpression' &&
+        node._babelType === 'MemberExpression'
+      ) {
+        console.log('here', init.arguments[0], node)
+
+        return (isUsed = isSameMemmberExpression(node, init.arguments[0]))
+      } else if (
+        init.arguments[0].type === 'Identifier' &&
+        node.type === 'Identifier' &&
+        !isInMemberExpression(node)
+      ) {
+        console.log('here', init.arguments[0], node)
+        return (isUsed = init.arguments[0].name === node.name)
+      }
+    }
+  })
+  isUsed && console.log('isUsed', isUsed, node)
+  if (!isUsed && scope.upper)
+    return (isUsed = isUsedInUseProxy(node, scope.upper))
+  return isUsed
+}
 
 function isInCallback(node) {
   if (!node.parent || !node.parent.type) return false
@@ -114,6 +144,7 @@ function isInRender(node) {
     return isInRender(node.parent)
   }
 }
+
 export default {
   meta: {
     type: 'problem',
@@ -128,18 +159,23 @@ export default {
       Identifier(node) {
         const scope = context.getScope(node)
         if (
-          (node.parent.type === 'AssignmentExpression' ||
-            (node.parent.type === 'MemberExpression' &&
-              node.parent.parent.type === 'AssignmentExpression')) &&
-          node.parent.object !== node &&
-          isUsedInUseProxy(node, scope)
+          (isInAssignmentExpression(node) &&
+            !isInMemberExpression(node) &&
+            isUsedInUseProxy(node, scope)) ||
+          (isInAssignmentExpression(node) &&
+            isInMemberExpression(node) &&
+            ((outerMemberExpression(node).property === node &&
+              isUsedInUseProxy(outerMemberExpression(node), scope)) ||
+              (node.parent.object === node &&
+                outerMemberExpression(node).property.type === 'Literal' &&
+                isUsedInUseProxy(outerMemberExpression(node), scope))))
         ) {
+          console.log(node)
           return context.report({
             node: node.parent.parent,
             message: UNEXPECTED_STATE_MUTATING,
           })
         }
-
         if (
           node.parent.type === 'MemberExpression' &&
           node.parent.property === node
