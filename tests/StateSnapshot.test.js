58bfcff6be129ebea0b874fb123bee8c72b2c79d
Reaper <ahoy@barelyhuman.dev>
2022-07-21 19:42:23 +0530

    fix: segregate read only to implementations
    moves isRead only to manually handle react hook primitives
    creates another helper to check if a value is being returned
    for a custom hook
    also added additional check to `isReadOnly` to avoid
    raising flags when working with callExpressions

--- tests/StateSnapshot.test.js
+++ tests/StateSnapshot.test.js
@@ -14,307 +14,352 @@ const ruleTester = new RuleTester({
 ruleTester.run('state-snapshot-rule', rule, {
   valid: [
     `
-      const state = proxy({ count: 0 })
-      state.count += 1
-      `,
+         const state = proxy({ count: 0 })
+         state.count += 1
+         `,
     `
-      const state = proxy({ count: 0})
-      function Counter() {
-        return (
-          <div>
-            <button onClick={() => ++state.count}>+1</button>
-          </div>
-        )
-      }
-      `,
+         const state = proxy({ count: 0})
+         function Counter() {
+           return (
+             <div>
+               <button onClick={() => ++state.count}>+1</button>
+             </div>
+           )
+         }
+         `,
     `
-      function Counter() {
-        const snapshot = useSnapshot(state)
-        const { count } = snapshot
-        return (
-          <div>{count} {snapshot.count}</div>
-        )
-      }
-      `,
+         function Counter() {
+           const snapshot = useSnapshot(state)
+           const { count } = snapshot
+           return (
+             <div>{count} {snapshot.count}</div>
+           )
+         }
+         `,
     `
-      function Counter() {
-        const { count } = useSnapshot(state)
-        return <div>{count}</div>
-      }
-      `,
+         function Counter() {
+           const { count } = useSnapshot(state)
+           return <div>{count}</div>
+         }
+         `,
     `
-      function Counter() {
-        const snap = useSnapshot(state)
-        useEffect(() => {
-          state.count += 1
-        }, [])
-        return <div>{snap.foo}</div>
-      }
-      `,
+         function Counter() {
+           const snap = useSnapshot(state)
+           useEffect(() => {
+             state.count += 1
+           }, [])
+           return <div>{snap.foo}</div>
+         }
+         `,
     `
-    function useExample2(s) {
-      const {b: {c} } = useSnapshot(s.a1);
+       function useExample2(s) {
+         const {b: {c} } = useSnapshot(s.a1);
+    
+         useEffect(() => {
+           if (c === 'a1c') {
+             state.a1.b.c = 'example';
+           }
+         }, [c]);
+       }`,
 
-      useEffect(() => {
-        if (c === 'a1c') {
-          state.a1.b.c = 'example';
-        }
-      }, [c]);
-    }`,
-
     `
-    function useProxyStateExample(a) {
-      const s = useSnapshot(a);
-
-      useEffect(() => {
-          a.a = 'example';
-      }, [s.a]);
-    }`,
+       function useProxyStateExample(a) {
+         const s = useSnapshot(a);
+    
+         useEffect(() => {
+             a.a = 'example';
+         }, [s.a]);
+       }`,
     `const state = proxy({ count: 0 });
-    function App() {
-      const snap = useSnapshot(state);
-      const handleClick = useCallback(() => {
-        console.log(snap.count);
-      }, [snap.count]);
-      return (
-        <div>
-          {snap.count} <button onClick={handleClick}>click</button>
-        </div>
-      );
+       function App() {
+         const snap = useSnapshot(state);
+         const handleClick = useCallback(() => {
+           console.log(snap.count);
+         }, [snap.count]);
+         return (
+           <div>
+             {snap.count} <button onClick={handleClick}>click</button>
+           </div>
+         );
+       }
+       `,
+    `
+       const DirectReadComponent = () => {
+         const debounceSnap = useSnapshot(state);
+         return <div>{debounceSnap}</div>;
+       };
+       `,
+    `
+       const ObjectPatternReadOne = () => {
+         const {b: {c} } = useSnapshot(state);
+         return <div>{c}</div>;
+       };`,
+    `
+       const ObjectPatternReadTwo = () => {
+         const {c} = useSnapshot(state);
+         return <div>{c}</div>;
+       };`,
+    `
+     const state = proxy({ count: 1 })
+     const useDoubled = () => {
+       const snap = useSnapshot(state)
+       return snap.count * 2
+     }
+     const Component = ()=> {
+       const doubled = useDoubled()
+       return <>{doubled}</>
+     }
+      `,
+    `
+    const Component = ()=>{
+      const { index } = useSnapshot(idxStore);
+    React.useEffect(() => {
+     const getUsers = async (id) => {
+       setUsers()
+     };
+     getUsers(index);
+  }, [index])
+  return <></>
     }
-    `,
+`,
     `
-    const DirectReadComponent = () => {
-      const debounceSnap = useSnapshot(state);
-      return <div>{debounceSnap}</div>;
-    };
-    `,
+	const state = proxy({count:1});
+	const useDoubled = () => {
+		const snap = useSnapshot(state)
+		const doubled = snap.count*2;
+		return doubled 
+	}
+	`,
     `
-    const ObjectPatternReadOne = () => {
-      const {b: {c} } = useSnapshot(state);
-      return <div>{c}</div>;
-    };`,
+	const state = proxy({count:1});
+	const useDoubled = () => {
+		const snap = useSnapshot(state)
+		const {doubled} = {doubled:snap.count*2};
+		return doubled 
+	}
+	`,
     `
-    const ObjectPatternReadTwo = () => {
-      const {c} = useSnapshot(state);
-      return <div>{c}</div>;
-    };`,
+const state = proxy({ count: 0 });
+const App=()=>{
+  const {count} = useSnapshot(state);
+  const doubled = count*2;
+  return (
+    <div>
+      {count} <button onClick={handleClick}>click</button>
+    </div>
+  );
+}
+`,
   ],
   invalid: [
     {
       code: `
-    const state = proxy({ count: 0})
-    function Counter() {
-      const { count } = state
-      return (
-        <div>
-          {state.count} {count}
-          <button onClick={() => ++state.count}>+1</button>
-        </div>
-      )
-    }
-    `,
+     const state = proxy({ count: 0})
+     function Counter() {
+       const { count } = state
+       return (
+         <div>
+           {state.count} {count}
+           <button onClick={() => ++state.count}>+1</button>
+         </div>
+       )
+     }
+     `,
       errors: [PROXY_RENDER_PHASE_MESSAGE, PROXY_RENDER_PHASE_MESSAGE],
     },
     {
       code: `
-    function Counter() {
-      const snapshot = useSnapshot(state)
-      useEffect(() => {
-        ++snapshot.count
-      })
-      return (
-        <div>{snapshot.count}</div>
-      )
-    }
-    `,
+     function Counter() {
+       const snapshot = useSnapshot(state)
+       useEffect(() => {
+         ++snapshot.count
+       })
+       return (
+         <div>{snapshot.count}</div>
+       )
+     }
+     `,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-    function Counter() {
-      const { count } = useSnapshot(state)
-      useEffect(() => {
-        ++count
-      })
-      return (
-        <div>{snapshot.count}</div>
-      )
-    }
-    `,
+     function Counter() {
+       const { count } = useSnapshot(state)
+       useEffect(() => {
+         ++count
+       })
+       return (
+         <div>{snapshot.count}</div>
+       )
+     }
+     `,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-    const state = proxy({ count: 0})
-    function Counter() {
-      const snap = useSnapshot(state)
-      return (
-        <div>
-          {snap.count}
-          <button onClick={() => ++snap.count}>+1</button>
-        </div>
-      )
-    }
-    `,
+     const state = proxy({ count: 0})
+     function Counter() {
+       const snap = useSnapshot(state)
+       return (
+         <div>
+           {snap.count}
+           <button onClick={() => ++snap.count}>+1</button>
+         </div>
+       )
+     }
+     `,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-      function Counter() {
-        const snap = useSnapshot(state.r.b)
-        const snap1 = useSnapshot(state)
-        const handleClick = () => {
-          state.r.b = ['hello', 'ss']
-          state = ['hello']
-        }
-       return <div></div> 
-      }
-      `,
+       function Counter() {
+         const snap = useSnapshot(state.r.b)
+         const snap1 = useSnapshot(state)
+         const handleClick = () => {
+           state.r.b = ['hello', 'ss']
+           state = ['hello']
+         }
+        return <div></div>
+       }
+       `,
       errors: [UNEXPECTED_STATE_MUTATING, UNEXPECTED_STATE_MUTATING],
     },
     {
       code: `
-          function Counter() {
-            const snap = useSnapshot(state)
-            const handleClick = () => {
-              state = ['hello']
-            }
-           return <div></div>
-          }
-          `,
+           function Counter() {
+             const snap = useSnapshot(state)
+             const handleClick = () => {
+               state = ['hello']
+             }
+            return <div></div>
+           }
+           `,
       errors: [UNEXPECTED_STATE_MUTATING],
     },
     {
       code: `
-          function Counter() {
-            const snap = useSnapshot(state.a)
-            const handleClick = () => {
-              state.a = ['hello']
-            }
-           return <div></div>
-          }
-          `,
+           function Counter() {
+             const snap = useSnapshot(state.a)
+             const handleClick = () => {
+               state.a = ['hello']
+             }
+            return <div></div>
+           }
+           `,
       errors: [UNEXPECTED_STATE_MUTATING],
     },
     {
       code: `
-          function Counter() {
-            const snap = useSnapshot(state[0])
-            return <div>{snap}<button onClick={() => ++state[0]}>inc</button></div>
-          }
-          `,
+           function Counter() {
+             const snap = useSnapshot(state[0])
+             return <div>{snap}<button onClick={() => ++state[0]}>inc</button></div>
+           }
+           `,
       errors: [UNEXPECTED_STATE_MUTATING],
     },
     {
       code: `const state = proxyWithComputed({
-      count: 0,
-    }, {
-      quadrupled: (snap) => snap.doubled * 2,
-      doubled: (snap) => snap.count * 2,
-    })
-      `,
+       count: 0,
+     }, {
+       quadrupled: (snap) => snap.doubled * 2,
+       doubled: (snap) => snap.count * 2,
+     })
+       `,
       errors: [COMPUTED_DECLARATION_ORDER],
     },
     {
       code: `const state = proxyWithComputed({
-      count: 0,
-    }, {
-      quadrupled: ({ doubled }) => doubled * 2,
-      doubled: (snap) => snap.count * 2,
-    })
-      `,
+       count: 0,
+     }, {
+       quadrupled: ({ doubled }) => doubled * 2,
+       doubled: (snap) => snap.count * 2,
+     })
+       `,
       errors: [COMPUTED_DECLARATION_ORDER],
     },
     {
       code: `function Counter() {
-      const snapshot = useSnapshot(state)
-      useEffect(() => {
-        snapshot.count = randomValue + 1;
-      })
-      return (
-        <></>
-      )
-    }`,
+       const snapshot = useSnapshot(state)
+       useEffect(() => {
+         snapshot.count = randomValue + 1;
+       })
+       return (
+         <></>
+       )
+     }`,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-          function useProxyStateExample(a) {
-            const s = useSnapshot(a);
-
-            useEffect(() => {
-              if(s.a==="1"){
-                s.a = 'example';
-              }
-            }, [s.a]);
-          }`,
+           function useProxyStateExample(a) {
+             const s = useSnapshot(a);
+             useEffect(() => {
+               if(s.a==="1"){
+                 s.a = 'example';
+               }
+             }, [s.a]);
+           }`,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-
-    function App() {
-      const snap = useSnapshot(state)
-      const handleClick = () => {
-        console.log(snap.count) // This is not recommended as it can be stale.
-      }
-      return (
-        <div>
-           <button onClick={handleClick}>click</button>
-        </div>
-      )
-    }
-
-          `,
+     function App() {
+       const snap = useSnapshot(state)
+       const handleClick = () => {
+         console.log(snap.count) // This is not recommended as it can be stale.
+       }
+       return (
+         <div>
+            <button onClick={handleClick}>click</button>
+         </div>
+       )
+     }
+           `,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-          const state = proxy({ count: 0 })
-
-          function App() {
-            const snap = useSnapshot(state)
-            const handleClick = useCallback(() => {
-              console.log(snap.count) // This is not recommended as it can be stale.
-            },[])
-            return (
-              <div>
-                {snap.count} <button onClick={handleClick}>click</button>
-              </div>
-            )
-          }
-          `,
+           const state = proxy({ count: 0 })
+           function App() {
+             const snap = useSnapshot(state)
+             const handleClick = useCallback(() => {
+               console.log(snap.count) // This is not recommended as it can be stale.
+             },[])
+             return (
+               <div>
+                 {snap.count} <button onClick={handleClick}>click</button>
+               </div>
+             )
+           }
+           `,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `const state = proxy({ count: 0 })
-
-          function App() {
-            const snap = useSnapshot(state)
-            const handleClick = useCallback(() => {
-              console.log(snap.count) // This is not recommended as it can be stale.
-            },[])
-            return (
-              <div>
-                {snap.count} <button onClick={handleClick}>click</button>
-              </div>
-            )
-          }
-    `,
+           function App() {
+             const snap = useSnapshot(state)
+             const handleClick = useCallback(() => {
+               console.log(snap.count) // This is not recommended as it can be stale.
+             },[])
+             return (
+               <div>
+                 {snap.count} <button onClick={handleClick}>click</button>
+               </div>
+             )
+           }
+     `,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
     {
       code: `
-          function useExample2(s) {
-            const {b: {c} } = useSnapshot(s.a1);
-
-            useEffect(() => {
-              if (c === 'a1c') {
-                state.a1.b.c = 'example';
-              }
-            }, []);
-          }`,
+           function useExample2(s) {
+             const {b: {c} } = useSnapshot(s.a1);
+             useEffect(() => {
+               if (c === 'a1c') {
+                 s.a1.b.c = 'example';
+               }
+             }, []);
+           }`,
       errors: [SNAPSHOT_CALLBACK_MESSAGE],
     },
   ],
