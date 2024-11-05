af193cad80ced94ec78434d8759ab8bfaee3a9fc
reaper <ahoy@barelyhuman.dev>
2024-11-05 19:57:23 +0530

    fix: add backward compat parser config

--- tests/parser-config.js
+++ tests/parser-config.js
@@ -1,4 +1,34 @@
+const eslintVersion = require('eslint/package.json').version
+const semver = require('semver')
+
 export function getParserConfig() {
+  if (semver.satisfies(eslintVersion, '>=9')) {
+    return getEslintv9Config()
+  }
+  return getEslintv8Config()
+}
+
+function getEslintv9Config() {
+  return {
+    languageOptions: {
+      parser: require('@babel/eslint-parser'),
+      ecmaVersion: 6,
+      sourceType: 'module',
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          babelrc: false,
+          configFile: false,
+          parserOpts: {
+            plugins: ['jsx'],
+          },
+        },
+      },
+    },
+  }
+}
+
+function getEslintv8Config() {
   return {
     parserOptions: {
       requireConfigFile: false,
